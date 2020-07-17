3253a33a301067ffd57d04ce7f47dc0b6fa15cbc
Snapster02 <alabson.inc@gmail.com>
2020-07-17 13:31:55 +0100

    Ready to deploy@firday

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,9 +1,13 @@
 // Sign-up Logic
+var loadingState = document.getElementById("loader")
+loadingState.style.visibility = "hidden"
+
 const signupForm = document.querySelector('#signupForm');
 signupForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
     // User info
+    loadingState.style.visibility = "visible"
     const fname = signupForm['signupFname'].value;
     const lname = signupForm['signupLname'].value;
     const userName = signupForm['signupUsername'].value;
@@ -17,11 +21,11 @@ signupForm.addEventListener('submit', (e) => {
    
     auth.createUserWithEmailAndPassword(email, password).then(user => {
         const newUser = auth.currentUser
-        alert(user.user.email, "Created")
+        console.log(user.user.email, "Created")
         newUser.updateProfile({
             displayName: userName
         }).then(() => {
-            alert("profile added")
+            console.log("profile added")
             db.collection("users").doc(newUser.uid.toString()).set({
                 fname: fname,
                 gender: gender,
@@ -29,12 +33,24 @@ signupForm.addEventListener('submit', (e) => {
                 number: phone,
                 zipCode: zipCode
             }).then(() => {
-                alert("document added");
+                console.log("document added");
                 signupForm.reset()
                 newUser.sendEmailVerification().then(function () {
                     alert("We just sent a massage to your mail, thank you for signing up")
+
+                    const fundObj = auth.currentUser
+                    db.collection("funds").doc(fundObj.uid.toString()).set({
+                        amount: 500,
+                        gain: 50,
+                        uid: fundObj.uid
+                    }).then(() => {
+                        console.log("Funds data successfully added!")
+                    }).catch(error => {
+                        console.log("Error occured")
+                    })
                     auth.signOut().then(() => {
                         console.log("SignOut successfully")
+                        loadingState.style.visibility = "hidden"
                         window.location.replace("../login.html")
                     }).catch((error) => {
                         console.log("Signout error")
@@ -50,12 +66,16 @@ signupForm.addEventListener('submit', (e) => {
         })
     }).catch(error => {
         alert(error)
-        let errcode = error.zipCode
+        let errcode = error.code
         let errMsg = error.message
         if (errCode == "auth/weak-password") {
-            alert('errCode')
+            console.log('errCode')
+            loadingState.style.visibility = "hidden"
         } else {
-            alert(errMsg)
+            console.log(errMsg)
+            loadingState.style.visibility = "hidden"
         }
+        loadingState.style.visibility = "hidden"
+        console.log(error)
     });
 });
--- scripts/authLogin.js
+++ scripts/authLogin.js
@@ -1,10 +1,14 @@
 // Login Logic
+var loadingState = document.getElementById("loader")
+loadingState.style.visibility = "hidden"
+
 const loginForm = document.querySelector('#loginForm');
 loginForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     // const loginEmail = loginForm['email'].val;
     // const loginPassword = loginForm['password'].val;
+    loadingState.style.visibility = "visible"
     const loginEmail = $("#email").val();
     const loginPassword = $("#password").val();
 
@@ -13,15 +17,19 @@ loginForm.addEventListener('submit', (e) => {
     auth.signInWithEmailAndPassword(loginEmail, loginPassword).then(() => {
         alert("Login Successfully")
         loginForm.reset()
+        loadingState.style.visibility = "hidden"
         window.location.replace("../payment.html")
     }).catch(error => {
         let errCode = error.errCode
         let errMsg = error.message
         if (errCode) {
             console.log(errCode)
+            // loadingState.style.visibility = "hidden"
         } else {
             console.log(errMsg)
+            // loadingState.style.visibility = "hidden"
         }
+        loadingState.style.visibility = "hidden"
         console.log(error)
     })
 })
